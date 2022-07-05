d9ad7c58057768795e8166ab7397a9c024aec4c6
worashf <uppert83@gmail.com>
2022-07-05 16:34:25 +0300

    add todo input

---
+++ src/components/InputTodo.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+
+class InputTodo extends Component {
+  state = {
+    title: "",
+  };
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+  handleSubmit = (e) => {
+    e.preventDefault();
+    if (this.state.title.trim()) {
+      this.props.addTodoItemProps(this.state.title);
+      this.setState({
+        title: "",
+      });
+    } else {
+      alert("please enter title");
+    }
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit} className="form-container">
+        <input
+          type="text"
+          placeholder="Add Todo..."
+          value={this.state.title}
+          name="title"
+          onChange={this.handleChange}
+          className="input-text"
+        />
+        <button className="input-submit">Submit</button>
+      </form>
+    );
+  }
+}
+export default InputTodo;
