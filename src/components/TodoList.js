ad5cc318c0aabf43e8faa5621dc34ddb166482c8
worashf <uppert83@gmail.com>
2022-07-05 16:37:23 +0300

    add todo list components

---
+++ src/components/TodoList.js
@@ -0,0 +1,20 @@
+import React from "react";
+import TodoItem from "./TodoItem";
+class TodoList extends React.Component {
+  render() {
+    return (
+      <ul>
+        {this.props.todos.map((todo) => (
+          <TodoItem
+            key={todo.id}
+            todo={todo}
+            handleChangeProps={this.props.handleChangeProps}
+            deleteItem={this.props.deleteItem}
+          />
+        ))}
+      </ul>
+    );
+  }
+}
+
+export default TodoList;
